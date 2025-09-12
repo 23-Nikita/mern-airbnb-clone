b62e4bbccf8bdf0ccd88088026bdb0de0291469c
23-Nikita <mittalnikita.23@gmail.com>
2025-09-13 00:30:18 +0530

    Add Listing model and sample data (data.js)

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
-
+const Listing = require('./models/listing.js');
 const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust";
 
 main()
@@ -11,13 +11,26 @@ main()
   console.log(err);
 });
 
-
 async function main() {
     await mongoose.connect(MONGO_URL);    
 }
 
 app.get("/" , (req,res)=>{
     res.send("it is root  ");
+})
+
+
+app.get("/testListing", async(req,res)=>{
+    let sampleListing = new Listing({
+        title: "My New Villa",
+        description: "By the beach",
+        price:1200,
+        location:"Calangute,Gos",
+        country:"India",
+    });
+    await sampleListing.save();
+    console.log("sample was saved");
+    res.send("sucessful testing");
 })
 
 app.listen(8080,()=>{
