f7627fd75298c3942006329f097d005e02ffb4c0
23-Nikita <mittalnikita.23@gmail.com>
2025-10-28 15:21:46 +0530

    Updated UI, fixed styling, and improved listing pages

--- app.js
+++ app.js
@@ -1,14 +1,19 @@
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
-const Listing = require('./models/listing.js');
 const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust";
 const path = require("path");
 const methodOverride = require("method-override");
 const ejsMate = require("ejs-mate");
-const wrapAsync = require("./utils/wrapAsync.js");
 const ExpressError= require("./utils/ExpressError.js");
-const {listingSchema} =require("./schema.js");
+const listingRouter = require("./routes/listing.js");
+const reviewRouter = require("./routes/review.js");
+const userRouter = require("./routes/user.js");
+const session = require("express-session");
+const flash = require("connect-flash");
+const Passport = require("passport");
+const LocalStrategy = require("passport-local");
+const  User = require("./models/user.js");
 
 
 
@@ -30,104 +35,58 @@ app.use(methodOverride("_method"));
 app.engine("ejs", ejsMate)
 app.use(express.static(path.join(__dirname,"public")));
 
+
+const sessionOptions ={
+    secret:"mySuperSecretCode",
+    resave: false,
+    saveUninitialized :true,
+    cookie:{
+        expires: Date.now()+7*24*60*60*1000,
+        maxAge:7*24*60*60*1000,
+        httpOnly : true,
+    }
+}
+
+app.use(session(sessionOptions));
+app.use(flash());
+
+app.use( Passport.initialize());
+app.use(Passport.session());
+Passport.use(new LocalStrategy(User.authenticate()));
+
+Passport.serializeUser(User.serializeUser());
+Passport.deserializeUser(User.deserializeUser());
+
 app.get("/" , (req,res)=>{
     res.send("it is root  ");
 });
 
-
-const validateListing = (req,res,next)=>{
-  let {error}= listingSchema.validate(req.body);  
-   if(error){
-    let errMsg= error.details.map((el)=>el.message).join(",");
-    throw new ExpressError(400,errMsg)
-   } else{
+app.use((req,res,next)=>{
+    res.locals.success=req.flash("success");
+    res.locals.error=req.flash("error");
+    res.locals.currUser= req.user;
     next();
-   }
-}
-
-//IndexRoute 
-app.get("/listings",wrapAsync(async(req,res)=>{
-   const allListings = await Listing.find({});
-   res.render("listings/index.ejs",{allListings} );
-})
-);
-
-//New Route
-
-app.get("/listings/new",(req,res)=>{
-    res.render("listings/new.ejs");
 })
 
-//show route 
 
-app.get("/listings/:id",wrapAsync(async(req,res)=>{
-    let {id}=req.params;
-    const listing = await Listing.findById(id);
-    res.render("listings/show.ejs",{listing});
-})
-);
-
-
-//Create Route
-app.post("/listings",
-    validateListing,
-    wrapAsync( async (req,res,next)=>{    
-   const newListing = new Listing(req.body.listing);
-   
-   await newListing.save();
-   res.redirect("/listings")
-  
-  
-})
-);
-
-//edit route 
-
-app.get("/listings/:id/edit", wrapAsync(async(req,res)=>{
-    let {id}=req.params;
-    const listing = await Listing.findById(id);
-    res.render("listings/edit.ejs",{listing});
-})
-);
-
-//update route
-
-app.put("/listings/:id",
-    validateListing,
-     wrapAsync(async(req,res)=>{   
-    let { id } = req.params;
-    await Listing.findByIdAndUpdate(id, {...req.body.listing});
-    res.redirect(`/listings/${id}`);
-
-}));
+app.get("/demouser", async(req,res)=>{
+    let fakeUser  = new User({
+        email:"student@gmail.com",
+        username:"delta-student"
+    })
+  let registeredUser =  await  User.register(fakeUser,"helloworld");
+  res.send(registeredUser);
+});
 
 
-//delete route 
-app.delete("/listings/:id", wrapAsync(async(req,res)=>{
-    let { id } = req.params;
-    let deletedListing = await Listing.findByIdAndDelete(id);
-    console.log(deletedListing);
-    res.redirect("/listings");
-}) );
+app.use("/listings", listingRouter);
+app.use("/listings/:id/reviews", reviewRouter);
+app.use("/", userRouter);
 
-
-// app.get("/testListing", async(req,res)=>{
-//     let sampleListing = new Listing({
-//         title: "My New Villa",
-//         description: "By the beach",
-//         price:1200,
-//         location:"Calangute,Gos",
-//         country:"India",
-//     });
-//     await sampleListing.save();
-//     console.log("sample was saved");
-//     res.send("sucessful testing");
-// })
 
 app.use((req,res,next)=>{
     next(new ExpressError(404,"Page Not Found !"));
 })
-
 
 app.use((err,req,res,next)=>{
     let {statusCode=500,message="Something Went Wrong!"} = err;
--- init/index.js
+++ init/index.js
@@ -18,6 +18,7 @@ async function main(){
 
 const initDB = async () =>{
    await Listing.deleteMany({});
+   initData.data = initData.data.map((obj)=>({...obj, owner:"68ff2b88657bfc98003b9abd"}))
    await Listing.insertMany(initData.data);
    console.log("data was initialized");
 };
